16b09e65c15095349f5d69b67558bc8f15716c4f
leichaojian <fzyz.abc@gmail.com>
2014-11-05 09:25:50 +0800

    add chapter7

---
+++ chapter7/7_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char **argv)
+{
+	int		isLower = 0;
+	int		isUpper = 0;
+	int		c;
+
+	if (argc != 2){
+		printf("argument should be 2\n");
+		return 1;
+	}
+
+	while (--argc && (*++argv)[0] == '-'){
+		switch (c = *(++*argv)){
+			case 'l':
+				isLower = 1;
+				break;
+			case 'u':
+				isUpper = 1;
+				break;
+			default:
+				printf("unknown command\n");
+				break;
+		}
+	}
+
+	while ((c = getchar()) != EOF){
+		if (isLower){
+			putchar(tolower(c));
+		}
+		else if (isUpper){
+			putchar(toupper(c));
+		}
+		else{
+			putchar(c);
+		}
+	}
+
+	return 0;
+}
